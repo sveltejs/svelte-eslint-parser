4bed4a4e3ec788f4590acd060ad1ed1e723e9ea5
IWANABETHATGUY <974153916@qq.com>
2021-04-27 20:37:45 +0800

    refactor: 💡 refactor sort (#11)

--- src/parser/sort.ts
+++ src/parser/sort.ts
@@ -3,18 +3,9 @@
  */
 export function sort<T extends { range: [number, number] }>(tokens: T[]): T[] {
     return tokens.sort((a, b) => {
-        if (a.range[0] > b.range[0]) {
-            return 1
+        if (a.range[0] !== b.range[0]) {
+            return a.range[0] - b.range[0]
         }
-        if (a.range[0] < b.range[0]) {
-            return -1
-        }
-        if (a.range[1] > b.range[1]) {
-            return 1
-        }
-        if (a.range[1] < b.range[1]) {
-            return -1
-        }
-        return 0
+        return a.range[1] - b.range[1]
     })
 }
