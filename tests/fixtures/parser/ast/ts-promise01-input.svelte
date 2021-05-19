5d494161f23bd2c7f7b71c4b37bfe1454c460642
yosuke ota <otameshiyo23@gmail.com>
2021-05-19 11:03:00 +0900

    Fix crash in {#await} on ts

---
+++ tests/fixtures/parser/ast/ts-promise01-input.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let promise:Promise<{a:number}> = new Promise((resolve)=>resolve({a:42}));
+</script>
+
+{#await promise}
+	<p>...waiting</p>
+{:then number}
+	<p>The number is {number.a}</p>
+{:catch error}
+	<p style="color: red">{error.message}</p>
+{/await}
