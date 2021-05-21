0dbfde117a6bc986d8f211ec2cba03863609c251
yosuke ota <otameshiyo23@gmail.com>
2021-05-21 17:43:26 +0900

    update

--- explorer-v2/svelte.config.js
+++ explorer-v2/svelte.config.js
@@ -1,6 +1,5 @@
 import staticAdapter from '@sveltejs/adapter-static';
 import { resolve } from 'path';
-import stringReplace from './build-system/vite-plugins/vite-plugin-string-replace.js';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -24,54 +23,10 @@ const config = {
 					path: resolve('./build-system/shim/path.js'),
 					fs: resolve('./build-system/shim/fs.js'),
 					'eslint-scope': resolve('./build-system/shim/eslint-scope.js'),
-					'svelte-eslint-parser': resolve('./build-system/shim/svelte-eslint-parser.js')
+					'svelte-eslint-parser': resolve('./build-system/shim/svelte-eslint-parser.js'),
+					'eslint-plugin-svelte3': resolve('./build-system/shim/eslint-plugin-svelte3.js')
 				}
-			},
-			build: {
-				// minify: true,
-				get commonjsOptions() {
-					return {
-						include: [
-							/node_modules/,
-							resolve('./build-system/shim/assert.js'),
-							resolve('./build-system/shim/eslint-scope.js'),
-							resolve('./build-system/shim/svelte-eslint-parser.js')
-						]
-					};
-				}
-			},
-			plugins: [
-				stringReplace({
-					test: /eslint-plugin-svelte3.js/u,
-					search: 'Object\\.keys\\(__require\\.cache\\)',
-					replace: (original) => `[] /* ${original} */`,
-					flags: ''
-				}),
-				stringReplace({
-					test: /eslint-plugin-svelte3.js/u,
-					search: '__dirname',
-					replace: (original) => `"" /* ${original} */`,
-					flags: 'g'
-				}),
-				stringReplace({
-					test: /eslint-plugin-svelte3.js/u,
-					search: '^',
-					replace: () => `import eslint4bLinter from "eslint4b";\n`,
-					flags: ''
-				}),
-				stringReplace({
-					test: /eslint-plugin-svelte3.js/u,
-					search: '__require\\(linter_path\\)',
-					replace: (original) => `{Linter:eslint4bLinter} // ${original}`,
-					flags: ''
-				}),
-				stringReplace({
-					test: /eslint-plugin-svelte3.js/u,
-					search: 'throw new Error\\("Could not find ESLint Linter in require cache"\\);',
-					replace: (original) => ` // ${original}`,
-					flags: ''
-				})
-			]
+			}
 		}
 	}
 };
