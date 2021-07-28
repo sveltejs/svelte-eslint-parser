4884ee7f1a5f474230132fe5cff3ea7bc3146f04
yosuke ota <otameshiyo23@gmail.com>
2021-07-28 15:56:37 +0900

    Support svelte 3.41.0

---
+++ tests/fixtures/parser/ast/await02-input.svelte
@@ -0,0 +1,17 @@
+<script>
+let expression = new Promise()
+</script>
+
+{#await expression catch}
+	<p>oh no! Something broke!</p>
+{/await}
+
+{#await expression then}
+	<p>the promise is resolved</p>
+{:catch theError}
+	<p>oh no! {theError.message}</p>
+{/await}
+
+{#await expression then}
+	<p>the promise is resolved</p>
+{/await}
