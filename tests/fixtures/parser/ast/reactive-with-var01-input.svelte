e16c147364f0e67fe0d2b1157e026c4074d48e40
Yosuke Ota <otameshiyo23@gmail.com>
2021-10-05 09:16:29 +0900

    Fix incorect scope for reactive statement with predefined variables. (#89)

---
+++ tests/fixtures/parser/ast/reactive-with-var01-input.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { count, count2 } from './stores.js';
+	import { writable } from 'svelte/store';
+	
+	let foo = 0
+	let bar = writable(0)
+	$: $count2 = $count * 2
+	$: foo = $count * 3
+	$: $bar = $count * 4
+	$: baz = $count * 5 // ComputedVariable
+</script>
+
+<h1>$count: {$count}</h1>
+<h1>$count2: {$count2}</h1>
+<h1>foo: {foo}</h1>
+<h1>$bar: {$bar}</h1>
+<h1>baz: {baz}</h1>
+<button on:click={()=>$count++}>+1</button>
