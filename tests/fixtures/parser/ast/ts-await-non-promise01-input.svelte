2cc9056d35d8ed5dfab400060061973926dfaa7a
yosuke ota <otameshiyo23@gmail.com>
2021-12-14 15:36:19 +0900

    Fix error when `{#await}` with literal

---
+++ tests/fixtures/parser/ast/ts-await-non-promise01-input.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	const str = 'abc' as const
+</script>
+
+{#await 1234 then number}
+	<p>The number is {number}</p>
+{/await}
+
+{#await str then s}
+	<p>The string is {s}</p>
+{/await}
+
+{#await str.slice(0) then s}
+	<p>The string is {s}</p>
+{/await}
