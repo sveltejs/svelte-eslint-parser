8a0712937a2c5e278b5307922a3f288e858d7c3f
ota-meshi <otameshiyo23@gmail.com>
2022-01-21 13:53:08 +0900

    fix site

--- explorer-v2/src/hooks.js
+++ explorer-v2/src/hooks.js
@@ -1,20 +1,4 @@
-import cookie from 'cookie';
-import { v4 as uuid } from '@lukeed/uuid';
-
-export async function handle({ request, resolve }) {
-	const cookies = cookie.parse(request.headers.cookie || '');
-	request.locals.userid = cookies.userid || uuid();
-
-	const response = await resolve(request);
-
-	if (!cookies.userid) {
-		// if this is the first time the user has visited this app,
-		// set a cookie so that we recognise them when they return
-		response.headers['set-cookie'] = cookie.serialize('userid', request.locals.userid, {
-			path: '/',
-			httpOnly: true
-		});
-	}
-
+export async function handle({ event, resolve }) {
+	const response = await resolve(event);
 	return response;
 }
