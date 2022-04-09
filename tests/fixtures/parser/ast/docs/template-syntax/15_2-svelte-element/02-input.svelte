bdcba5c3d0a1f87d3abf9792436ffa22612242aa
Yosuke Ota <otameshiyo23@gmail.com>
2022-04-09 09:28:07 +0900

    Support for `<svelte:element>` (#156)

---
+++ tests/fixtures/parser/ast/docs/template-syntax/15_2-svelte-element/02-input.svelte
@@ -0,0 +1,6 @@
+<script>
+	let tag = 'div';
+	export let handler;
+</script>
+
+<svelte:element this={tag} on:click={handler}>Foo</svelte:element>
---
+++ tests/fixtures/parser/ast/svelte-element-with-bind-this-input.svelte
@@ -0,0 +1,7 @@
+<script>
+	const tag = 'div';
+	let a;
+	$: console.log(a)
+</script>
+
+<svelte:element this={tag} bind:this={a}>Foo</svelte:element>
