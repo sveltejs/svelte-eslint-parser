3876861791e1ec4a570f76ae62f0611278abcc88
yosuke ota <otameshiyo23@gmail.com>
2022-07-22 08:09:12 +0900

    add testcase

---
+++ tests/fixtures/parser/ast/reactive-with-var02-input.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { writable } from 'svelte/store';
+	
+	const foo = writable(0)
+	const bar = writable(0)
+	$: $foo = $bar * 2
+</script>
+
+<h1>$foo: {$foo}</h1>
+<h1>$bar: {$bar}</h1>
+<button on:click={()=>$foo++}>$foo++</button>
+<button on:click={()=>$bar++}>$bar++</button>
