cde0812ee5d6815647ea28f1e053777cab4f2c1c
yosuke ota <otameshiyo23@gmail.com>
2022-07-21 22:41:06 +0900

    fix site

--- explorer-v2/svelte.config.js
+++ explorer-v2/svelte.config.js
@@ -1,5 +1,4 @@
 import staticAdapter from '@sveltejs/adapter-static';
-import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,24 +14,8 @@ const config = {
 			assets: 'build',
 			fallback: null
 		}),
-		vite: {
-			server: {
-				fs: { strict: false }
-			},
-			resolve: {
-				alias: {
-					assert: resolve('./build-system/shim/assert.js'),
-					path: resolve('./build-system/shim/path.js'),
-					fs: resolve('./build-system/shim/fs.js'),
-					module: resolve('./build-system/shim/module.js'),
-
-					globby: resolve('./build-system/shim/globby.js'),
-					tslib: resolve('./node_modules/tslib/tslib.es6.js'),
-					eslint: resolve('./build-system/shim/eslint.js'),
-					'svelte-eslint-parser': resolve('./build-system/shim/svelte-eslint-parser.js'),
-					'eslint-plugin-svelte3': resolve('./build-system/shim/eslint-plugin-svelte3.js')
-				}
-			}
+		prerender: {
+			default: true
 		}
 	}
 };
---
+++ explorer-v2/vite.config.js
@@ -0,0 +1,26 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { resolve } from 'path';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	server: {
+		fs: { strict: false }
+	},
+	resolve: {
+		alias: {
+			assert: resolve('./build-system/shim/assert.js'),
+			path: resolve('./build-system/shim/path.js'),
+			fs: resolve('./build-system/shim/fs.js'),
+			module: resolve('./build-system/shim/module.js'),
+
+			globby: resolve('./build-system/shim/globby.js'),
+			tslib: resolve('./node_modules/tslib/tslib.es6.js'),
+			eslint: resolve('./build-system/shim/eslint.js'),
+			'svelte-eslint-parser': resolve('./build-system/shim/svelte-eslint-parser.js'),
+			'eslint-plugin-svelte3': resolve('./build-system/shim/eslint-plugin-svelte3.js')
+		}
+	}
+};
+
+export default config;
