746f4259a6b14fbefac1b61fc1f204aca4e49bc8
yosuke ota <otameshiyo23@gmail.com>
2022-07-25 23:18:08 +0900

    add test case

---
+++ tests/fixtures/parser/ast/ts-store02-type-output.svelte
@@ -0,0 +1,11 @@
+<script context="module" lang="ts">
+	import { writable } from 'svelte/store'; // writable: <T>(value?: T, start?: StartStopNotifier<T>) => Writable<T>, writable: <T>(value?: T, start?: StartStopNotifier<T>) => Writable<T>
+	const a = writable(0) // a: Writable<number>, writable(0): Writable<number>
+	declare const $a: string // $a: string
+</script>
+<script lang="ts">
+	const b = writable(0) // b: Writable<number>, writable(0): Writable<number>
+	declare const $b: string // $b: string
+</script>
+{$a} <!-- $a: string -->
+{$b} <!-- $b: string -->
