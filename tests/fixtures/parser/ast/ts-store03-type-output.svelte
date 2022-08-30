5091949624116a9ab134e66d6b97a2d9afbbcc32
yosuke ota <otameshiyo23@gmail.com>
2022-08-30 12:53:56 +0900

    fix: wrong store access type information

---
+++ tests/fixtures/parser/ast/ts-store03-type-output.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Writable, Readable } from 'svelte/store'; // Writable: any, Writable: any, Readable: any, Readable: any
+	let maybeUndef: Writable<number> | undefined // maybeUndef: Writable<number> | undefined
+	let maybeNull: Readable<string> | null // maybeNull: Readable<string> | null
+	let maybeNullAndStr: Readable<boolean> | null | string // maybeNullAndStr: string | Readable<boolean> | null
+
+	export function fn() { // fn: () => void
+		$maybeUndef; // $maybeUndef: number | undefined
+		$maybeNull; // $maybeNull: string | null
+		$maybeNullAndStr; // $maybeNullAndStr: string | boolean | null
+	}
+</script>
+{$maybeUndef} <!-- $maybeUndef: number | undefined -->
+{$maybeNull} <!-- $maybeNull: string | null -->
+{$maybeNullAndStr} <!-- $maybeNullAndStr: string | boolean | null -->
