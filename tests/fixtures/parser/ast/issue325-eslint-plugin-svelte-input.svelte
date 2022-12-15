4d67f05077d8f126a5c876756e93f734704fca08
yosuke ota <otameshiyo23@gmail.com>
2022-12-15 10:05:06 +0900

    fix: virtual references remained

---
+++ tests/fixtures/parser/ast/issue325-eslint-plugin-svelte-input.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    export let message: string|undefined;
+    export const formatMessage = async (message: string): Promise<string> => {
+        return new Promise(resolve => {
+        resolve(message);
+        });
+    };
+</script>
+
+{#if !message}
+    No message
+{:else}
+    {#await formatMessage(message)}
+        Loading...
+    {:then result}
+        {result}
+    {/await}
+{/if}
