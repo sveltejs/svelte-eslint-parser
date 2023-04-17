1db5f7df1b142991bcd40658d240c92e76a5fce5
Marek Dědič <developer@dedic.eu>
2023-04-17 21:18:13 +0200

    Fixed ESLintCompatiblePostCSSNode for Container subclasses

--- src/ast/style.ts
+++ src/ast/style.ts
@@ -4,16 +4,17 @@ import type { BaseNode } from "./base";
 export type ESLintCompatiblePostCSSNode<PostCSSNode extends Node = Node> =
   BaseNode &
     (PostCSSNode extends Container<infer Child>
-      ? Omit<
-          Container<ESLintCompatiblePostCSSNode<Child>>,
-          "parent" | "walk"
-        > & {
-          parent: BaseNode;
-          walk(
-            callback: (
-              node: ESLintCompatiblePostCSSNode<Child>,
-              index: number
-            ) => false | void
-          ): false | undefined;
-        }
+      ? Omit<PostCSSNode, "parent" | "type"> &
+          Omit<
+            Container<ESLintCompatiblePostCSSNode<Child>>,
+            "parent" | "walk"
+          > & {
+            parent: BaseNode;
+            walk(
+              callback: (
+                node: ESLintCompatiblePostCSSNode<Child>,
+                index: number
+              ) => false | void
+            ): false | undefined;
+          }
       : PostCSSNode);
