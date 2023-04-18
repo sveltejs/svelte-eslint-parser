2bf21258d28f6ceaf80b42b4d07486ad3caa3e2b
Marek Dědič <developer@dedic.eu>
2023-04-18 17:26:40 +0200

    Removed uneeded re-implementation of postcss Container

--- src/ast/style.ts
+++ src/ast/style.ts
@@ -1,61 +1,11 @@
-import type { Node, ChildProps, Container, Root } from "postcss";
+import type { Container, Node, Root } from "postcss";
 import type { Locations } from "./common";
 import type { SvelteStyleElement } from "./html";
 
-type ESLintCompatiblePostCSSContainer<
-  PostCSSNode extends Node,
-  Child extends Node
-> = {
-  each(
-    callback: (
-      node: ESLintCompatiblePostCSSNode<Child>,
-      index: number
-    ) => false | void
-  ): false | undefined;
-  every(
-    condition: (
-      node: ESLintCompatiblePostCSSNode<Child>,
-      index: number,
-      nodes: ESLintCompatiblePostCSSNode<Child>[]
-    ) => boolean
-  ): boolean;
-  get first(): ESLintCompatiblePostCSSNode<Child> | undefined;
-  index(child: ESLintCompatiblePostCSSNode<Child> | number): number;
-  insertAfter(
-    oldNode: ESLintCompatiblePostCSSNode<Child> | number,
-    newNode:
-      | ESLintCompatiblePostCSSNode<Child>
-      | ChildProps
-      | string
-      | ESLintCompatiblePostCSSNode<Child>[]
-      | ChildProps[]
-      | string[]
-  ): ESLintCompatiblePostCSSNode<PostCSSNode>;
-  insertBefore(
-    oldNode: ESLintCompatiblePostCSSNode<Child> | number,
-    newNode:
-      | ESLintCompatiblePostCSSNode<Child>
-      | ChildProps
-      | string
-      | ESLintCompatiblePostCSSNode<Child>[]
-      | ChildProps[]
-      | string[]
-  ): ESLintCompatiblePostCSSNode<PostCSSNode>;
-  get last(): ESLintCompatiblePostCSSNode<Child> | undefined;
-  nodes: ESLintCompatiblePostCSSNode<Child>[];
-  push(
-    child: ESLintCompatiblePostCSSNode<Child>
-  ): ESLintCompatiblePostCSSNode<PostCSSNode>;
-  removeChild(
-    child: ESLintCompatiblePostCSSNode<Child> | number
-  ): ESLintCompatiblePostCSSNode<PostCSSNode>;
-  some(
-    condition: (
-      node: ESLintCompatiblePostCSSNode<Child>,
-      index: number,
-      nodes: ESLintCompatiblePostCSSNode<Child>[]
-    ) => boolean
-  ): boolean;
+type ESLintCompatiblePostCSSContainer<Child extends Node> = Omit<
+  Container<ESLintCompatiblePostCSSNode<Child>>,
+  "parent" | "type" | "walk"
+> & {
   walk(
     callback: (
       node: ESLintCompatiblePostCSSNode<Child>,
@@ -68,26 +18,10 @@ export type ESLintCompatiblePostCSSNode<PostCSSNode extends Node> =
   // The following hack makes the `type` property work for type narrowing, see microsoft/TypeScript#53887.
   PostCSSNode extends any
     ? Locations &
-        Omit<
-          PostCSSNode,
-          | "parent"
-          | "type"
-          | "each"
-          | "every"
-          | "first"
-          | "index"
-          | "insertAfter"
-          | "insertBefore"
-          | "last"
-          | "nodes"
-          | "push"
-          | "removeChild"
-          | "some"
-          | "walk"
-        > & {
+        Omit<PostCSSNode, "parent" | "type" | "walk"> & {
           type: `SvelteStyle-${PostCSSNode["type"]}`;
         } & (PostCSSNode extends Container<infer Child>
-          ? ESLintCompatiblePostCSSContainer<PostCSSNode, Child>
+          ? ESLintCompatiblePostCSSContainer<Child>
           : unknown) &
         (PostCSSNode extends Root
           ? {
