1b573fd54ddf7bbfc7a4d7fb98c398f90228b49b
yosuke ota <otameshiyo23@gmail.com>
2023-05-08 19:34:17 +0900

    fix: script

--- tools/update-meta.ts
+++ tools/update-meta.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import { ESLint } from "eslint";
 import { name, version } from "../package.json";
+import { getNewVersion } from "./lib/changesets-util";
 
 const META_PATH = path.resolve(__dirname, "../src/meta.ts");
 
@@ -17,9 +18,18 @@ async function main() {
  * in order to update its content execute "yarn build:meta"
  */
 export const name = ${JSON.stringify(name)} as const;
-export const version = ${JSON.stringify(version)} as const;
+export const version = ${JSON.stringify(await getVersion())} as const;
 `,
     { filePath: META_PATH }
   );
   fs.writeFileSync(META_PATH, result.output!);
+}
+
+/** Get version */
+function getVersion() {
+  // eslint-disable-next-line no-process-env -- ignore
+  if (process.env.IN_VERSION_CI_SCRIPT) {
+    return getNewVersion();
+  }
+  return version;
 }
