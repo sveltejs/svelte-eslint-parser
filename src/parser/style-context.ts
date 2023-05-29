229a72917ba5ccee163aa00108d3990cdc35489b
Marek Dědič <developer@dedic.eu>
2023-05-29 11:20:43 +0200

    Using a discriminated union for StyleContext

--- src/parser/style-context.ts
+++ src/parser/style-context.ts
@@ -5,9 +5,31 @@ import { parse as SCSSparse } from "postcss-scss";
 import type { Context } from "../context";
 import type { SourceLocation, SvelteStyleElement } from "../ast";
 
-export interface StyleContext {
-  sourceLang: string | null;
-  sourceAst: Root | null;
+export type StyleContext =
+  | StyleContextNoStyleElement
+  | StyleContextParseError
+  | StyleContextSuccess
+  | StyleContextUnknownLang;
+
+export interface StyleContextNoStyleElement {
+  status: "no-style-element";
+}
+
+export interface StyleContextParseError {
+  status: "parse-error";
+  sourceLang: string;
+  error: any;
+}
+
+export interface StyleContextSuccess {
+  status: "success";
+  sourceLang: string;
+  sourceAst: Root;
+}
+
+export interface StyleContextUnknownLang {
+  status: "unknown-lang";
+  sourceLang: string;
 }
 
 /**
@@ -17,14 +39,10 @@ export function parseStyleContext(
   styleElement: SvelteStyleElement | undefined,
   ctx: Context
 ): StyleContext {
-  const styleContext: StyleContext = {
-    sourceLang: null,
-    sourceAst: null,
-  };
   if (!styleElement || !styleElement.endTag) {
-    return styleContext;
+    return { status: "no-style-element" };
   }
-  styleContext.sourceLang = "css";
+  let sourceLang = "css";
   for (const attribute of styleElement.startTag.attributes) {
     if (
       attribute.type === "SvelteAttribute" &&
@@ -32,11 +50,11 @@ export function parseStyleContext(
       attribute.value.length > 0 &&
       attribute.value[0].type === "SvelteLiteral"
     ) {
-      styleContext.sourceLang = attribute.value[0].value;
+      sourceLang = attribute.value[0].value;
     }
   }
-  let parseFn: Parser<Root>;
-  switch (styleContext.sourceLang) {
+  let parseFn: Parser<Root>, sourceAst: Root;
+  switch (sourceLang) {
     case "css":
       parseFn = postcss.parse;
       break;
@@ -44,31 +62,31 @@ export function parseStyleContext(
       parseFn = SCSSparse;
       break;
     default:
-      return styleContext;
+      return { status: "unknown-lang", sourceLang };
   }
   const styleCode = ctx.code.slice(
     styleElement.startTag.range[1],
     styleElement.endTag.range[0]
   );
   try {
-    styleContext.sourceAst = parseFn(styleCode, {
+    sourceAst = parseFn(styleCode, {
       from: ctx.parserOptions.filePath,
     });
     // eslint-disable-next-line no-empty -- Catching errors is not a good way to go around this, a safe parser should be used instead.
   } catch {}
   fixPostCSSNodeLocation(
-    styleContext.sourceAst,
+    sourceAst,
     styleElement.loc,
     styleElement.startTag.range[1]
   );
-  styleContext.sourceAst.walk((node) =>
+  sourceAst.walk((node) =>
     fixPostCSSNodeLocation(
       node,
       styleElement.loc,
       styleElement.startTag.range[1]
     )
   );
-  return styleContext;
+  return { status: "success", sourceLang, sourceAst };
 }
 
 /**
