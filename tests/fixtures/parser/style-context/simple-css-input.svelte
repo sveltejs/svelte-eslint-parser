5e4f2f6ce6d9fe5afafaa3df80d837574522899b
Marek Dědič <developer@dedic.eu>
2023-05-30 16:05:31 +0200

    Added a PostCSS AST test with simple CSS

---
+++ tests/fixtures/parser/style-context/simple-css-input.svelte
@@ -0,0 +1,17 @@
+<script>
+  let a = 10
+</script>
+
+<span class="myClass">Hello!</span>
+
+<b>{a}</b>
+
+<style>
+  .myClass {
+    color: red;
+  }
+
+  b {
+    font-size: xx-large;
+  }
+</style>
