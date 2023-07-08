468fa92c5a94c9bbcb895349575ead06a53699e1
Yosuke Ota <otameshiyo23@gmail.com>
2023-07-08 08:38:27 +0000

    fix: wrong type information for `#await` with same id

---
+++ tests/fixtures/integrations/type-info-tests/await-with-same-id-input.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    let a: Promise<{ x: number }>
+    let b: Promise<{ x: number }>
+    let c: Promise<{ x: number }>
+    let d: Promise<number[]>
+    let e: Promise<number[]>
+</script>
+
+{#await a}
+    <div>await</div>
+{:then a}
+    <div>{a.x}</div>
+{/await}
+
+{#await b}
+    <div>await</div>
+{:then { x: b = 42 }}
+    <div>{b.toExponential()}</div>
+{/await}
+
+{#await c}
+    <div>await</div>
+{:then {...c}}
+    <div>{c.x}</div>
+{/await}
+
+{#await d}
+    <div>await</div>
+{:then [d]}
+    <div>{d.toExponential()}</div>
+{/await}
+
+{#await e}
+    <div>await</div>
+{:then [...e]}
+    <div>{e[0].toExponential()}</div>
+{/await}
---
+++ tests/fixtures/integrations/type-info-tests/await-with-same-id-setup.ts
@@ -0,0 +1,24 @@
+/* eslint eslint-comments/require-description: 0, @typescript-eslint/explicit-module-boundary-types: 0 */
+import type { Linter } from "eslint";
+import { generateParserOptions } from "../../../src/parser/test-utils";
+import { rules } from "@typescript-eslint/eslint-plugin";
+export function setupLinter(linter: Linter) {
+  linter.defineRule(
+    "@typescript-eslint/no-unsafe-member-access",
+    rules["no-unsafe-member-access"] as never
+  );
+}
+
+export function getConfig() {
+  return {
+    parser: "svelte-eslint-parser",
+    parserOptions: generateParserOptions(),
+    rules: {
+      "@typescript-eslint/no-unsafe-member-access": "error",
+    },
+    env: {
+      browser: true,
+      es2021: true,
+    },
+  };
+}
