c8adb0b0ba254638bc2f5518e0ad31326912e848
baseballyama <xydybaseball@gmail.com>
2023-11-23 13:47:52 +0900

    fix type error

--- src/parser/converts/attr.ts
+++ src/parser/converts/attr.ts
@@ -946,7 +946,7 @@ function buildExpressionTypeChecker<T extends ESTree.Expression>(
 }
 
 function hasTypeInfo(element: any): boolean {
-  if (element.type.startsWith("TS")) {
+  if (element.type?.startsWith("TS")) {
     return true;
   }
   for (const key of Object.keys(element)) {
