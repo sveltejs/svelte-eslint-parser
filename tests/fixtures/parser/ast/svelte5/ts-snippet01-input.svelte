79c194461addae0f0ddacf9190d28e6ffcc2d1ca
baseballyama <xydybaseball@gmail.com>
2023-11-23 13:32:03 +0900

    adding tests

---
+++ tests/fixtures/parser/ast/svelte5/ts-snippet01-input.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let msg = ""
+</script>
+
+{#snippet foo(msg: string)}
+	<p>{msg}</p>
+{/snippet}
+
+{@render foo(msg)}
---
+++ tests/fixtures/parser/ast/svelte5/ts-snippet01-type-output.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let msg = "" // msg: string
+</script>
+
+{#snippet foo(msg: string)} <!-- foo: (msg: string) => void, msg: string -->
+	<p>{msg}</p> <!-- msg: string -->
+{/snippet}
+
+{@render foo(msg)} <!-- foo: (msg: string) => void, msg: string -->
