6369e152d1eb3bbcb9629f1a1d23eb6dcb179de4
ota-meshi <otameshiyo23@gmail.com>
2024-02-25 09:18:34 +0900

    feat: add support for `$derived.by` type

---
+++ tests/fixtures/parser/ast/svelte5/ts-$derived-by01-type-output.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	const numbers = $state([1, 2, 3]); // numbers: number[], $state([1, 2, 3]): number[]
+	const total = $derived.by(() => { // total: number, $derived.by(() => { let total = 0; for (const n of numbers) { total += n; } return total; }): number
+		let total = 0;
+		for (const n of numbers) {
+			total += n;
+		}
+		return total;
+	});
+</script>
+
+<button on:click={() => numbers.push(numbers.length + 1)}> <!-- numbers.push(numbers.length + 1): number -->
+	{numbers.join(' + ')} = {total} <!-- numbers.join(' + '): string, total: number -->
+</button>
