894ae23cf585abeeebc58970de4a8aa7daf8c00a
ota-meshi <otameshiyo23@gmail.com>
2024-04-09 08:54:23 +0900

    fix: wrong token when using `lang=ts`

---
+++ tests/fixtures/parser/ast/ts-tokens-bug-input.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    // Copied from https://github.com/sveltejs/svelte-eslint-parser/issues/502
+    import Button from "$lib/Button.svelte";
+</script>
+<Button active={$store.number === index}>
+    {index} {tab}
+</Button>
