32df5e943943a8eec8719dfa60b721f18779dfe8
ota-meshi <otameshiyo23@gmail.com>
2024-04-09 09:26:16 +0900

    fix

--- tests/fixtures/parser/ast/ts-tokens-bug-input.svelte
+++ tests/fixtures/parser/ast/ts-tokens-bug-input.svelte
@@ -1,5 +1,5 @@
+<!-- Copied from https://github.com/sveltejs/svelte-eslint-parser/issues/502 -->
 <script lang="ts">
-    // Copied from https://github.com/sveltejs/svelte-eslint-parser/issues/502
     import Button from "$lib/Button.svelte";
 </script>
 <Button active={$store.number === index}>
