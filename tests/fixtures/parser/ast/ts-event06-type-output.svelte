86c781d782cc2a309280da82d6abe3bc38853f18
yosuke ota <otameshiyo23@gmail.com>
2024-06-09 21:12:26 +0900

    chore: update typescript-eslint-parser-for-extra-files to 0.7

--- tests/fixtures/parser/ast/ts-event06-type-output.svelte
+++ tests/fixtures/parser/ast/ts-event06-type-output.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-    import Component from './ts-event03-input.svelte'; // Component: typeof TsEvent03Input__SvelteComponent_
+    import Component from './ts-event03-input.svelte'; // Component: __sveltets_2_IsomorphicComponent<{}, { foo: CustomEvent<number>; bar: CustomEvent<string>; } & { [evt: string]: CustomEvent<any>; }, {}, Record<string, any>, string>
 </script>
 
-<Component on:foo="{e=>{ // Component: typeof TsEvent03Input__SvelteComponent_, e: CustomEvent<number>
+<Component on:foo="{e=>{ // Component: __sveltets_2_IsomorphicComponent<{}, { foo: CustomEvent<number>; bar: CustomEvent<string>; } & { [evt: string]: CustomEvent<any>; }, {}, Record<string, any>, string>, e: CustomEvent<number>
     // e.detail is number
     e.detail; // e.detail: number
 }}" />
