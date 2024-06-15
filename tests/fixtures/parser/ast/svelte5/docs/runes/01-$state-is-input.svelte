9fe1a461528e31f25f864e47420725f616c48502
baseballyama <xydybaseball@gmail.com>
2024-06-15 21:28:14 +0900

    add tests

---
+++ tests/fixtures/parser/ast/svelte5/docs/runes/01-$state-is-input.svelte
@@ -0,0 +1,9 @@
+<script>
+  let foo = $state({});
+  let bar = {};
+
+  foo.bar = bar;
+
+  console.log(foo.bar === bar); // false — `foo.bar` is a reactive proxy
+  console.log($state.is(foo.bar, bar)); // true
+</script>
---
+++ tests/fixtures/parser/ast/svelte5/docs/runes/03-2-$effect-tracking-input.svelte
@@ -0,0 +1,10 @@
+<script>
+  console.log("in component setup:", $effect.tracking()); // false
+
+  $effect(() => {
+    console.log("in effect:", $effect.tracking()); // true
+  });
+</script>
+
+<p>in template: {$effect.tracking()}</p>
+<!-- true -->
