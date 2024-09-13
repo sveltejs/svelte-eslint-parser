76e094ef15a5ff8c7f28a26d7452dd0d176fc9b1
yosuke ota <otameshiyo23@gmail.com>
2024-09-13 14:01:19 +0900

    fix

--- src/parser/svelte-ast-types-for-v5.ts
+++ src/parser/svelte-ast-types-for-v5.ts
@@ -1,7 +1,3 @@
-// FIXME Since the node type is not provided by "svelte/compiler",
-// we work around this by extracting the type from the parse function.
-// See also https://github.com/sveltejs/svelte/issues/12292
-
 import type { AST } from "svelte/compiler";
 
 export type Root = AST.Root;
