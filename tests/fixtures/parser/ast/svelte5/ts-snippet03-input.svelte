cc951ea7635f2620e94a052c8f1922b69ab2f122
yosuke ota <otameshiyo23@gmail.com>
2024-10-16 23:52:40 +0900

    fix: error in snippets with type annotations

---
+++ tests/fixtures/parser/ast/svelte5/ts-snippet03-input.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	function hello() {
+		console.log('Hello');
+	}
+</script>
+
+{@render foo({ onclick: hello })}
+
+{#snippet foo(o: {onclick: () => void})}
+  <button on:click={o.onclick}>Click me</button>
+{/snippet}
