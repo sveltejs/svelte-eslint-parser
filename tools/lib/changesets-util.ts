912f922f4ab7ad56af55a479a7d03e8b93ec0763
Yosuke Ota <otameshiyo23@gmail.com>
2024-12-02 09:19:21 +0900

    chore: fix release script (#610)

--- tools/lib/changesets-util.ts
+++ tools/lib/changesets-util.ts
@@ -1,9 +1,11 @@
 import getReleasePlan from "@changesets/get-release-plan";
 import path from "path";
 
+const dirname = path.dirname(new URL(import.meta.url).pathname);
+
 /** Get new version string from changesets */
 export async function getNewVersion(): Promise<string> {
-  const releasePlan = await getReleasePlan(path.resolve(__dirname, "../.."));
+  const releasePlan = await getReleasePlan(path.resolve(dirname, "../.."));
 
   return releasePlan.releases.find(
     ({ name }) => name === "svelte-eslint-parser",
