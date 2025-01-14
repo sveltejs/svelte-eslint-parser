6280a13bd0d8ae406dd2263bdbd02b52c441c65f
yosuke ota <otameshiyo23@gmail.com>
2025-01-14 18:47:44 +0900

    feat: replace `declaration` property of SvelteConstTag with `declarations` property

--- src/parser/converts/const.ts
+++ src/parser/converts/const.ts
@@ -13,14 +13,28 @@ export function convertConstTag(
   const mustache: SvelteConstTag = {
     type: "SvelteConstTag",
     declaration: null as any,
+    declarations: [] as any,
     parent,
     ...ctx.getConvertLocation(node),
   };
+
+  // Link declaration and declarations for backward compatibility.
+  // TODO Remove in v2 and later.
+  Object.defineProperty(mustache, "declaration", {
+    get() {
+      return mustache.declarations[0];
+    },
+    set(value) {
+      mustache.declarations = [value];
+    },
+    enumerable: false,
+  });
+
   ctx.scriptLet.addVariableDeclarator(
     getDeclaratorFromConstTag(node),
     mustache,
     (declaration) => {
-      mustache.declaration = declaration;
+      mustache.declarations = [declaration];
     },
   );
   const atConstStart = ctx.code.indexOf("@const", mustache.range[0]);
